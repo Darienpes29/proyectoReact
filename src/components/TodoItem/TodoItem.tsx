bcc31cf0d90f806ae128d5b8fb8fdaa901e5cc2b
Darienpes29 <darienpesca29@gmail.com>
2024-07-10 10:22:23 -0500

    Se comenta código css del archivo 'TodoItem.css' y se elimina contenedor div en el archivo 'TodoItem.tsx'.

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -18,21 +18,19 @@ const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed, category }) =
   };
 
   return (
-    <div style={{backgroundColor: 'transparent'}}>
-      <div className='todo-item'>
-        <h5>{id}</h5>
-        <h3>Task: {title}</h3>
-        <p>Category: {category}</p>
-          <div style={{ display: 'flex', justifyContent: 'center' }}  >
-          <p className={isChecked ? 'completed' : 'not-completed'} style={{ textDecoration: isChecked ? 'line-through' : 'none' , marginRight: '5px'}}>
-            {isChecked ? 'Completed' : 'Not Completed'}
-          </p>
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={handleCheckboxChange}
-            />
-          </div>
+    <div className='todo-item'>
+      <h5>{id}</h5>
+      <h3>Task: {title}</h3>
+      <p>Category: {category}</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}  >
+        <p className={isChecked ? 'completed' : 'not-completed'} style={{ textDecoration: isChecked ? 'line-through' : 'none' , marginRight: '5px'}}>
+          {isChecked ? 'Completed' : 'Not Completed'}
+        </p>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+          />
       </div>
     </div>
   );
